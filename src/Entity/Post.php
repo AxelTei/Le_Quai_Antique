41c25c58574fa6ff491865747c7bcfc8e305743e
Axel Teisseire <mr.teisseire@gmail.com>
2023-02-03 17:45:30 +0100

    create first entity Post.php

---
+++ src/Entity/Post.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+class Post
+{
+    private int $id;
+    private ?string $title = NULL;
+    private string $content;
+    private string $image;
+    private $user;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function setContent($content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
