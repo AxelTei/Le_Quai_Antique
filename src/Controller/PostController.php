39a88ed5518e26f6acb584583443afdb1c0e186a
Axel Teisseire <mr.teisseire@gmail.com>
2023-02-03 18:20:43 +0100

    create the twig template for the form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,16 @@ class PostController extends AbstractController
     {
         return $this->render('post/index.html.twig', [
             'age' => 30,
+        ]);
+    }
+
+    #[Route('/post/new')]
+    public function create(): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        return $this->render('post/form.html.twig', [
+            'post_form' => $form->createView()
         ]);
     }
 }
