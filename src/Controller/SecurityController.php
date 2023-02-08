396728ecc0dc9acd39e460627a921e644041728e
Axel Teisseire <mr.teisseire@gmail.com>
2023-02-08 19:24:14 +0100

    try to generate the form in a formtype not directly in twig but failed it, the test is add in comment for a retry

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,15 +12,22 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUserName = $authenticationUtils->getLastUsername();
 
+        // $form = $this->createForm(LoginType::class);
+        // $form->handleRequest($request);
+        // if($form->isSubmitted() && $form->isValid())
+        // {
+        // }
+
         return $this->render('security/login.html.twig', [
             'error' => $error,
             'last_username' => $lastUserName,
+            // "form" => $form->createView()
         ]);
     }
 
---
+++ src/Form/LoginType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Url;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("email", TextType::class, [
+                "label" => "Email",
+                "required" => true,
+                "attr" => array('name' => '_username'),
+                "constraints" => [
+                    new Length(['min' => 5, 'max' => 180, 'minMessage' => "Veuillez saisir votre Email.", 'maxMessage' => "Votre Email ne doit pas faire plus de 180 caractères !"]),
+                    new NotBlank(["message" => "Veuillez saisir votre Email."])    
+                ]
+            ])
+            ->add("password", PasswordType::class, [
+                "label" => "Mot de passe",
+                "required" => true,
+                "constraints" => [
+                    new NotBlank(["message" => "Veuillez saisir votre Mot de passe."])    
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            "data_class" => User::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'post_item'
+        ]);
+    }
+}
