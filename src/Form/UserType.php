bf18a2aa7eb0a18b378b88215f1abb8c257f8ba1
Axel Teisseire <mr.teisseire@gmail.com>
2023-02-09 18:41:59 +0100

    change textype in email form from UserType.php in EmailType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +19,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("email", TextType::class, [
+            ->add("email", EmailType::class, [
                 "label" => "Email",
                 "required" => true,
                 "constraints" => [
