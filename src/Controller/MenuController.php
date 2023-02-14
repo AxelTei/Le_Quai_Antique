13c57f1ff1dbf09dfd850435863fadbf464608b5
Axel Teisseire <mr.teisseire@gmail.com>
2023-02-14 17:21:45 +0100

    add new dish structure in menu/index.html.twig

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Menu;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/carte', name: 'carte')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $repository = $doctrine->getRepository(Menu::class);
+        $menus = $repository->findAll(); // SELECT * FROM `post`;
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            "posts" => $menus
         ]);
     }
 }
