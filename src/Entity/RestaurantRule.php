ee5e8e3bee351cc06a3336208f65dfb2c921c7a3
Axel Teisseire <mr.teisseire@gmail.com>
2023-03-05 18:07:34 +0100

    add restaurantRule_form to /booking/index.html.twig

--- src/Entity/RestaurantRule.php
+++ src/Entity/RestaurantRule.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\RestaurantRuleRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RestaurantRuleRepository::class)]
