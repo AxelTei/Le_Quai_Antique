3908b3c9c39856ddf73cf17d946da01e6ac738b2
Axel Teisseire <mr.teisseire@gmail.com>
2023-03-20 19:17:54 +0100

    comment a wrong command in the second migration created on this app, to migrate to heroku server without failing

--- migrations/Version20230213181150.php
+++ migrations/Version20230213181150.php
@@ -17,12 +17,12 @@ final class Version20230213181150 extends AbstractMigration
         return 'Change User table in Customers and add Menu table';
     }
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        // $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DC3568B40 FOREIGN KEY (customers_id) REFERENCES customers (id) ON DELETE CASCADE');
-        // $this->addSql('CREATE INDEX IDX_5A8A6C8DC3568B40 ON post (customers_id)');
-    }
+    // public function up(Schema $schema): void
+    // {
+    //     // this up() migration is auto-generated, please modify it to your needs
+    //     // $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DC3568B40 FOREIGN KEY (customers_id) REFERENCES customers (id) ON DELETE CASCADE');
+    //     // $this->addSql('CREATE INDEX IDX_5A8A6C8DC3568B40 ON post (customers_id)');
+    // }
 
     public function down(Schema $schema): void
     {
