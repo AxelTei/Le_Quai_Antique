b9640b05d052391fb651c7d0b366aa3f43f2b223
Axel Teisseire <mr.teisseire@gmail.com>
2023-11-20 15:40:18 +0100

    create admin count

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -21,6 +21,7 @@ class CustomerController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
+            $customer->setRoles('ROLE_ADMIN');
             $em = $doctrine->getManager();
             $em->persist($customer);
             $em->flush();
