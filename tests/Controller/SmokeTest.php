6d411c13bad87c8b657f72c77735fce21558f867
Axel Teisseire <mr.teisseire@gmail.com>
2024-05-23 19:39:31 +0200

    add a few test before making them work with testfixtures and create test environment, allow web-profiler to work in test mode

---
+++ tests/Controller/SmokeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Repository\CustomerRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SmokeTest extends WebTestCase
+{
+    // public function testVisitingWhileLoggedIn(): void
+    // {
+    //     $client = static::createClient();
+    //     $userRepository = static::getContainer()->get(CustomerRepository::class);
+
+    //     // retrieve the test user
+    //     $testUser = $userRepository->findOneByEmail('test@test.com');
+
+    //     // simulate $testUser being logged in
+    //     $client->loginUser($testUser);
+
+    //     // test e.g. the profile page
+    //     $client->request('GET', '/'); //à remplacer avec la page réservation
+    //     $this->assertResponseIsSuccessful();
+    //     // $this->assertSelectorTextContains('h1', 'Hello John!');
+    // }
+
+    public function testIfBookingJsRouteIsSuccessfulWithTheRightData(): void
+    {
+        $client = self::createClient();
+
+        $client->request('POST', '/bookJS', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            "alias" => "Test",
+            "date" => "2024-05-31", // changer de date pour le test le cas échéant
+            "hourSelectedDay" => "12:00",
+            "hourSelectedNight" => "Non",
+            "phoneNumber" => "0601020304",
+            "preferedGroupNumber" => 2,
+            "allergies" => "test"
+        ], JSON_THROW_ON_ERROR));
+
+        $statusCode = $client->getResponse()->getStatusCode();
+        $this->assertEquals(200, $statusCode); // créer les fixtures Restaurant_Places et Rule pour faire marcher le test
+    }
+}
