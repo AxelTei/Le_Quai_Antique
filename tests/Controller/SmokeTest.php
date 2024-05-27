4723ea3b8a5f848469d3e6e7628bcbd95b1820e5
Axel Teisseire <mr.teisseire@gmail.com>
2024-05-27 17:04:32 +0200

    add new fixtures and advanced fixtures and successfully test the smokeTest written to work with fixtures on DataBase Test

--- tests/Controller/SmokeTest.php
+++ tests/Controller/SmokeTest.php
@@ -32,7 +32,7 @@ class SmokeTest extends WebTestCase
             'CONTENT_TYPE' => 'application/json',
         ], json_encode([
             "alias" => "Test",
-            "date" => "2024-05-31", // changer de date pour le test le cas échéant
+            "date" => "2024-07-04",
             "hourSelectedDay" => "12:00",
             "hourSelectedNight" => "Non",
             "phoneNumber" => "0601020304",
@@ -41,6 +41,6 @@ class SmokeTest extends WebTestCase
         ], JSON_THROW_ON_ERROR));
 
         $statusCode = $client->getResponse()->getStatusCode();
-        $this->assertEquals(200, $statusCode); // créer les fixtures Restaurant_Places et Rule pour faire marcher le test
+        $this->assertEquals(201, $statusCode); // Test if is successfully fulfilled
     }
 }
